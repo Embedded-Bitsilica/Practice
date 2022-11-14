c28d8c98eaf1c9c620fe0b5a145fcf6a392e6ed4
Embedded-Bitsilica <embeddedbitsilica@gmail.com>
2022-11-14 17:41:53 +0530

    dj-dharma

--- 1.c
+++ 1.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
+#include<signal.h>
 int main()
 {
 	printf("Hello...\n");
 	signal(2,SIG_IGN);
 	signal(3,SIG_IGN);
+	printf("bahut jagah hai\n");
 	while(1);
 }
--- ranjeet.c
+++ ranjeet.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-printf("This is my first file\n"
+printf("This is my first file\n");
+
+printf("Sab galat hai babua\n");
 return 0;
 }
